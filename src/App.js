f8a5c955487805cf3fda36296c5cb0fd9d3f4026
ctd-frontend <92804884+ctd-frontend@users.noreply.github.com>
2022-06-13 15:41:40 -0300

    upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// El componente App es el padre de:
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+
+function App() {
+
+  return (
+    <div className="App">
+      <Cabecera />
+      <Listado />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cabecera.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// El componente Cabera no tiene componentes hijos.
+// ESTADO: Cabecera no tiene estado.
+// MÉTODOS: Cabecera no tiene métodos.
+// PROPS: Cabecera recibe de su padre la cantidad que va a mostrar dentro del span correpondiente
+// Maqueta de Cabecera:
+//    h1
+//    p > span     (el span mostrará la cantidad recibida por props)
+
+export default function Cabecera() {
+  
+  return (
+    <header>
+        {/* maquetar Cabecera aquí */}
+    </header>
+  )
+}
